3866e515d0f84e9cbf76b771ba2a1679fc0ef3fb
Hunvely <jihoon9611@gmail.com>
2024-03-07 12:01:19 +0900

    edit lambda code

--- user/UserRepository.java
+++ user/UserRepository.java
@@ -2,12 +2,32 @@ package user;
 
 import crawler.CrawlerRepository;
 
+import java.sql.*;
+
 public class UserRepository {
 
     private static UserRepository instance = new UserRepository();
 
     public static UserRepository getInstance() { // 정적 생성 메서드
         return instance;
+    }
+
+    public void findUsers() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/roddb";
+        String userName = "roddb";
+        String password = "roddb";
+
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from board");
+
+        resultSet.next();
+        String name = resultSet.getString("name");
+        System.out.println(name);
+
+        resultSet.close();
+        statement.close();
+        connection.close();
     }
 
 }
