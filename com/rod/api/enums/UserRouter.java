e3d868b1bf8edaa3f1197eff0e7cee755dcdc2e9
Hunvely <jihoon9611@gmail.com>
2024-03-13 20:04:59 +0900

    Userview code -> UserRouter

---
+++ com/rod/api/enums/UserRouter.java
@@ -0,0 +1,97 @@
+package com.rod.api.enums;
+
+import com.rod.api.user.UserController;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public enum UserRouter {
+
+    exit("x", (scanner, ctrl) -> {
+        return false;
+    }),
+    signUp("su", (scanner, ctrl) -> {
+        System.out.println("Input : | ID | Password | Name | PhoneNumber | Job | Height | Weight | ");
+        try {
+            ctrl.save(scanner);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }), signIn("si", (scanner, ctrl) -> {
+        ctrl.login(scanner);
+        return true;
+    }), searchId("fi", (scanner, ctrl) -> {
+        ctrl.getOne(scanner);
+        return true;
+    }), updatePassword("up", (scanner, ctrl) -> {
+        ctrl.updatePassword(scanner);
+        return true;
+    }), deleteAccount("da", (scanner, ctrl) -> {
+        ctrl.delete(scanner);
+        return true;
+    }), userList("ls", ((scanner, ctrl) -> {
+        try {
+            ctrl.findUsers();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    })), searchName("fn", (scanner, ctrl) -> {
+        ctrl.findUsersByName(scanner);
+        return true;
+    }), searchJob("fj", (scanner, ctrl) -> {
+        ctrl.findUsersByJob(scanner);
+        return true;
+    }), numberOfUsers("nou", (scanner, ctrl) -> {
+        ctrl.count();
+        return true;
+    }), touch("touch", (scanner, ctrl) -> {
+        try {
+            ctrl.createTable();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }), rm("rm", (scanner, ctrl) -> {
+        try {
+            ctrl.dropTable();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    });
+
+    private final String menu;
+    private final BiPredicate<Scanner, UserController> biPredicate;
+
+    UserRouter(String menu, BiPredicate<Scanner, UserController> biPredicate) {
+        this.menu = menu;
+        this.biPredicate = biPredicate;
+    }
+
+    public static Boolean userRoute(Scanner input, UserController userCtrl) {
+        System.out.println("=== x.Exit\n" +
+                "su.Sign up\n" +
+                "si.Sigh in\n" +
+                "fi.Search ID\n" +
+                "up.Update password\n" +
+                "da.Delete account\n" +
+                "ls.User list\n" +
+                "fn.Search name\n" +
+                "fj.Search job\n" +
+                "nou.Number of users\n" +
+                "touch.Create table\n" +
+                "rm.Delete table ===");
+        String msg = input.next();
+        return Stream.of(values())
+                .filter(i -> i.menu.equals(msg))
+                .findAny().orElseThrow(() -> new IllegalArgumentException("잘못된 입력입니다.")).biPredicate.test(input, userCtrl);
+
+    }
+
+
+}
--- com/rod/api/user/UserView.java
+++ com/rod/api/user/UserView.java
@@ -1,5 +1,7 @@
 package com.rod.api.user;
 
+import com.rod.api.enums.UserRouter;
+
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
@@ -9,86 +11,7 @@ public class UserView {
         UserController userController = new UserController();
         String msg = userController.addUsers();
         System.out.println(" addUsers 결과 : " + msg);
-        while (true) {
-            System.out.println("x.Exit\n" +
-                    "1.Sign up\n" +
-                    "2.Sigh in\n" +
-                    "3.Search ID\n" +
-                    "4.Update password\n" +
-                    "5.Delete account\n" +
-                    "ls.User list\n" +
-                    "7.Search name\n" +
-                    "8.Search job\n" +
-                    "9.Number of users\n" +
-                    "touch.Create table\n" +
-                    "rm.Delete table");
-            switch (input.next()) {
-                case "0":
-                    System.out.println("Exit");
-                    return;
-                case "1":
-                    System.out.println("1.Sign up");
-                    System.out.println(" | ID | Password | Name | PhoneNumber | Job | Height | Weight | ");
-                    System.out.println(userController.save(input));
-                    break;
-                case "2":
-                    System.out.println("2.Sigh in");
-                    System.out.println(" | ID | Password | Input ");
-                    System.out.println(userController.login(input));
-                    break;
-                case "3":
-                    System.out.println("3.Search ID");
-                    System.out.println(userController.getOne(input));
+        while (UserRouter.userRoute(input, userController)) ;
 
-                    break;
-                case "4":
-                    System.out.println("4.Update password");
-                    System.out.println(" | ID | Password | SSN | Input.");
-                    System.out.println(userController.updatePassword(input));
-                    break;
-                case "5":
-                    System.out.println("5.Delete account");
-                    System.out.println(" 탈퇴하실 ID를 입력하세요.");
-                    System.out.println(userController.delete(input));
-                    break;
-                case "ls":
-                    System.out.println("6.User list");
-                    System.out.println("회원목록을 조회합니다.");
-                    /*Map<String, ?> users = userController.getUserMap();
-                    users.forEach((k, v) -> {
-                        System.out.printf("아이디: %s, 회원정보: %s", k, v);
-                    });*/
-                    List<?> findUsers = userController.findUsers();
-                    System.out.println(findUsers);
-                    break;
-                case "7":
-                    System.out.println("7.Search name");
-                    System.out.println(" 검색할 회원의 이름을 입력하세요");
-                    userController.findUsersByName(input).forEach((i) -> {
-                        System.out.println(i);
-                    });
-                    break;
-                case "8":
-                    System.out.println("8.Search job");
-                    System.out.println(" 검색할 회원의 직업을 입력하세요");
-                    userController.findUsersByJob(input).forEach((i) -> {
-                        System.out.println(i);
-                    });
-                    break;
-                case "9":
-                    System.out.println("9.Number of users");
-                    msg = userController.count();
-                    System.out.println("Number of users : " + msg);
-                    break;
-                case "touch":
-                    System.out.println("touch.Create table");
-                    System.out.println(userController.createTable());
-                    break;
-                case "rm":
-                    System.out.println("rm.Delete table");
-                    System.out.println(userController.dropTable());
-                    break;
-            }
-        }
     }
 }
